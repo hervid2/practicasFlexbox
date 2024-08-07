c972adf79a22a7fc1f1f7ee2553a5d804ddc1dc9
hervid2 <hdcardonab@gmail.com>
2024-08-06 20:51:21 -0500

    header6-08-2024

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const link =  document.querySelectorAll('.nav__link');
+const input = document.querySelector(".nav__input");
+
+link.forEach(element => {
+    element.addEventListener("click",function(event) {
+        if(input.checked){
+            input.checked = false
+        }
+    })
+    
+});
